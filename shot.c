e7748c6ecf36cff052ea9168f1bc53a7e9b42e65
Ji Hwan An <angeljihwan@gmail.com>
2024-12-16 18:46:29 +0900

    shot: check conflict logics

--- shot.c
+++ shot.c
@@ -39,13 +39,18 @@ static size_t GLOBAL_TXS_COUNT = 0;
 static resource RESOURCES[MAX_RESOURCES] = {0};
 static size_t RESOURCES_COUNT = 0;
 
-int tx_should_compare(const tx *t1, const tx *t2) {
+const int tx_should_compare(const tx *t1, const tx *t2) {
   return t2->start_time < t1->start_time && t1->start_time < t2->commit_time;
 }
 
-int tx_resolve_conflict(const tx *t1, const tx *t2) {
-  (void)t1;
-  (void)t2;
+const int tx_check_conflict(const tx *tx) {
+  for (size_t i = 0; i < tx->actions_count; i++) {
+    const time_t resource_write_time = tx->actions[i].rs->write_time;
+    if (tx->start_time <= resource_write_time &&
+        resource_write_time <= tx->commit_time) {
+      return 1;
+    }
+  }
 
   return 0;
 }
@@ -64,6 +69,9 @@ void tx_commit(tx *tx) {
   for (size_t i = 0; i < GLOBAL_TXS_COUNT; i++) {
     if (tx_should_compare(tx, &GLOBAL_TXS[i])) {
       printf("Should compare %s and %s\n", tx->name, GLOBAL_TXS[i].name);
+      if (tx_check_conflict(tx)) {
+        // abort
+      }
     }
   }
 }
