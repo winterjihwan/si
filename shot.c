3c7f571945e2b9cef3f50262668ef4fca22474d3
Ji Hwan An <angeljihwan@gmail.com>
2024-12-16 18:34:18 +0900

    shot: add resource types

--- shot.c
+++ shot.c
@@ -4,12 +4,20 @@
 
 #define MAX_TIME 0x7FFFFFFE
 #define MAX_GLOBAL_TXS 10
+#define MAX_RESOURCES 5
 #define MAX_ACTIONS_COUNT 10
+
+typedef struct {
+  char *data;
+  time_t write_time;
+  time_t read_time;
+} resource;
 
 typedef enum { READ, WRITE } action_t;
 typedef struct {
   time_t time;
   action_t type;
+  resource *rs;
 } action;
 
 typedef enum { STARTED, COMMITTED, ENDED } tx_state;
@@ -26,17 +34,26 @@ typedef struct {
 } tx;
 
 static time_t TIME = 0x01;
-static tx GLOBAL_TXS[10] = {0};
+static tx GLOBAL_TXS[MAX_GLOBAL_TXS] = {0};
 static size_t GLOBAL_TXS_COUNT = 0;
+static resource RESOURCES[MAX_RESOURCES] = {0};
+static size_t RESOURCES_COUNT = 0;
 
 int tx_should_compare(const tx *t1, const tx *t2) {
-  return t2->state == STARTED && (t1->start_time < t2->start_time) &&
-         (t2->start_time < t1->commit_time);
+  return t2->start_time < t1->start_time && t1->start_time < t2->commit_time;
 }
 
-void tx_read(tx *tx) {
+int tx_resolve_conflict(const tx *t1, const tx *t2) {
+  (void)t1;
+  (void)t2;
+
+  return 0;
+}
+
+void tx_read(tx *tx, resource *rs) {
   assert(tx->actions_count + 1 <= MAX_ACTIONS_COUNT);
-  const action act = {.time = TIME++, .type = READ};
+
+  const action act = {.time = TIME++, .type = READ, .rs = rs};
   tx->actions[tx->actions_count++] = act;
 }
 
@@ -86,27 +103,46 @@ inline static tx *tx_new(char *name) {
   assert(GLOBAL_TXS_COUNT + 1 < MAX_GLOBAL_TXS);
 
   const size_t tx_idx = GLOBAL_TXS_COUNT;
+  const time_t now = TIME;
+
   const tx tx = {.state = STARTED,
                  .name = name,
-                 .start_time = TIME++,
-                 .commit_time = MAX_TIME,
-                 .end_time = MAX_TIME,
+                 .start_time = now,
+                 .commit_time = now,
+                 .end_time = now,
                  .actions = {0},
                  .actions_count = 0};
   GLOBAL_TXS[GLOBAL_TXS_COUNT++] = tx;
+  TIME++;
 
   return &GLOBAL_TXS[tx_idx];
+}
+
+inline static resource *resource_new(char *data) {
+  assert(RESOURCES_COUNT + 1 < MAX_RESOURCES);
+
+  const size_t rs_idx = RESOURCES_COUNT;
+  const time_t now = TIME;
+
+  const resource rs = {.data = data, .write_time = now, .read_time = now};
+  RESOURCES[RESOURCES_COUNT++] = rs;
+  TIME++;
+
+  return &RESOURCES[rs_idx];
 }
 
 int main(void) {
   tx *t1 = tx_new("T1");
 
-  tx_read(t1);
+  resource *r1 = resource_new("Resource A");
+
+  tx_read(t1, r1);
 
   tx *t2 = tx_new("T2");
 
-  tx_read(t2);
+  tx_read(t2, r1);
   tx_commit(t1);
+  tx_commit(t2);
 
   global_txs_dump();
 }
