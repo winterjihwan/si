0a0cdb54cf11c022e26a77791080309aa31c2d8c
Ji Hwan An <angeljihwan@gmail.com>
2024-12-16 19:16:59 +0900

    log: add stable storage for logging

--- shot.c
+++ shot.c
@@ -1,49 +1,65 @@
 #include "assert.h"
 #include "time.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_TIME 0x7FFFFFFE
 #define MAX_GLOBAL_TXS 10
 #define MAX_RESOURCES 5
-#define MAX_ACTIONS_COUNT 10
+#define MAX_ACTIONS 10
+#define MAX_LOGS 100
 
 typedef struct {
   char *data;
   time_t write_time;
   time_t read_time;
-} resource;
+} Resource;
 
 typedef enum { READ, WRITE } action_t;
 typedef struct {
   time_t time;
   action_t type;
-  resource *rs;
-} action;
+  Resource *rs;
+} Action;
 
+typedef long tx_id;
 typedef enum { STARTED, COMMITTED, ENDED } tx_state;
 typedef struct {
   char *name;
+  tx_id id;
   tx_state state;
 
   time_t start_time;
   time_t commit_time;
   time_t end_time;
 
-  action actions[10];
+  Action actions[10];
   size_t actions_count;
-} tx;
+} Tx;
+
+typedef enum { WRITE_LOG, READ_LOG } log_t;
+typedef struct {
+  log_t type;
+  time_t time;
+  tx_id tx_id;
+  Resource *rs;
+  char *prev;
+  char *after;
+} Log;
 
 static time_t TIME = 0x01;
-static tx GLOBAL_TXS[MAX_GLOBAL_TXS] = {0};
+static Tx GLOBAL_TXS[MAX_GLOBAL_TXS] = {0};
 static size_t GLOBAL_TXS_COUNT = 0;
-static resource RESOURCES[MAX_RESOURCES] = {0};
+static Resource RESOURCES[MAX_RESOURCES] = {0};
 static size_t RESOURCES_COUNT = 0;
+static Log STABLE_STORAGE[MAX_LOGS];
+static size_t STABLE_STORAGE_COUNT = 0;
 
-const int tx_should_compare(const tx *t1, const tx *t2) {
+int tx_should_compare(const Tx *t1, const Tx *t2) {
   return t2->start_time < t1->start_time && t1->start_time < t2->commit_time;
 }
 
-const int tx_check_conflict(const tx *tx) {
+int tx_is_conflict(const Tx *tx) {
   for (size_t i = 0; i < tx->actions_count; i++) {
     const time_t resource_write_time = tx->actions[i].rs->write_time;
     if (tx->start_time <= resource_write_time &&
@@ -55,21 +71,35 @@ const int tx_check_conflict(const tx *tx) {
   return 0;
 }
 
-void tx_read(tx *tx, resource *rs) {
-  assert(tx->actions_count + 1 <= MAX_ACTIONS_COUNT);
+inline static void log_store(Log log) {
+  assert(STABLE_STORAGE_COUNT + 1 <= MAX_LOGS);
+  STABLE_STORAGE[STABLE_STORAGE_COUNT++] = log;
+}
 
-  const action act = {.time = TIME++, .type = READ, .rs = rs};
+void tx_read(Tx *tx, Resource *rs) {
+  assert(tx->actions_count + 1 <= MAX_ACTIONS);
+
+  const Log log = {
+      .type = READ_LOG,
+      .tx_id = tx->id,
+      .rs = rs,
+      .time = TIME++,
+  };
+  log_store(log);
+
+  const Action act = {.time = TIME++, .type = READ, .rs = rs};
+
   tx->actions[tx->actions_count++] = act;
 }
 
-void tx_commit(tx *tx) {
+void tx_commit(Tx *tx) {
   tx->commit_time = TIME++;
   tx->state = COMMITTED;
 
   for (size_t i = 0; i < GLOBAL_TXS_COUNT; i++) {
     if (tx_should_compare(tx, &GLOBAL_TXS[i])) {
       printf("Should compare %s and %s\n", tx->name, GLOBAL_TXS[i].name);
-      if (tx_check_conflict(tx)) {
+      if (tx_is_conflict(tx)) {
         // abort
       }
     }
@@ -87,7 +117,18 @@ const char *action_t_to_string(const action_t action_t) {
   }
 }
 
-void tx_print(tx *tx) {
+const char *log_t_to_string(const log_t log_t) {
+  switch (log_t) {
+  case READ_LOG:
+    return "read";
+  case WRITE_LOG:
+    return "write";
+  default:
+    return "undefined";
+  }
+}
+
+void tx_print(Tx *tx) {
   printf("%s\n", tx->name);
   printf("Started: %ld\n", (long)tx->start_time);
   if (tx->state >= COMMITTED)
@@ -101,19 +142,38 @@ void tx_print(tx *tx) {
   }
 }
 
+void log_print(Log *log) {
+  printf("Log at time: %ld\n", log->time);
+  printf("\ttype: %s\n", log_t_to_string(log->type));
+  printf("\ttx id: %ld\n", log->tx_id);
+  printf("\tresource: %s\n", log->rs->data);
+  if (log->type == WRITE_LOG) {
+    printf("\tprev: %s\n", log->prev);
+    printf("\tafter: %s\n", log->after);
+  }
+  printf("\n");
+}
+
 void global_txs_dump(void) {
   for (size_t i = 0; i < GLOBAL_TXS_COUNT; i++) {
     tx_print(&GLOBAL_TXS[i]);
   }
 }
 
-inline static tx *tx_new(char *name) {
+void stable_storage_dump(void) {
+  for (size_t i = 0; i < STABLE_STORAGE_COUNT; i++) {
+    log_print(&STABLE_STORAGE[i]);
+  }
+}
+
+inline static Tx *tx_new(char *name) {
   assert(GLOBAL_TXS_COUNT + 1 < MAX_GLOBAL_TXS);
 
   const size_t tx_idx = GLOBAL_TXS_COUNT;
   const time_t now = TIME;
 
-  const tx tx = {.state = STARTED,
+  const Tx tx = {.state = STARTED,
+                 .id = rand(),
                  .name = name,
                  .start_time = now,
                  .commit_time = now,
@@ -126,13 +186,13 @@ inline static tx *tx_new(char *name) {
   return &GLOBAL_TXS[tx_idx];
 }
 
-inline static resource *resource_new(char *data) {
+inline static Resource *resource_new(char *data) {
   assert(RESOURCES_COUNT + 1 < MAX_RESOURCES);
 
   const size_t rs_idx = RESOURCES_COUNT;
   const time_t now = TIME;
 
-  const resource rs = {.data = data, .write_time = now, .read_time = now};
+  const Resource rs = {.data = data, .write_time = now, .read_time = now};
   RESOURCES[RESOURCES_COUNT++] = rs;
   TIME++;
 
@@ -140,17 +200,18 @@ inline static resource *resource_new(char *data) {
 }
 
 int main(void) {
-  tx *t1 = tx_new("T1");
+  Tx *t1 = tx_new("T1");
 
-  resource *r1 = resource_new("Resource A");
+  Resource *r1 = resource_new("Resource A");
 
   tx_read(t1, r1);
 
-  tx *t2 = tx_new("T2");
+  Tx *t2 = tx_new("T2");
 
   tx_read(t2, r1);
   tx_commit(t1);
   tx_commit(t2);
 
+  stable_storage_dump();
   global_txs_dump();
 }
